a1920ab760a3511d24ddb0ffb4e9044f87e84437
nikhilgla <nikhil.baghel.32@gmail.com>
2023-11-02 12:56:14 +0530

    signup page

---
+++ signup.js
@@ -0,0 +1,12 @@
+console.log("JS start");
+
+
+
+// const myForm = document.querySelector('.my-form');
+// myForm.addEventListener('button', onSubmit);
+
+
+
+async function onSubmit(){
+    console.log("inside submit button");
+}
